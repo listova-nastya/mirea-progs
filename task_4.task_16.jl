f44a62d3c74f9caec5dca4f90d7e1181244489c3
listova-nastya <90141208+listova-nastya@users.noreply.github.com>
2022-06-01 21:36:05 +0300

    Delete task_4.task_16.jl

--- task_4.task_16.jl
+++
@@ -1,9 +0,0 @@
-#ДАНО: Робот - в произвольной клетке ограниченного прямоугольного поля с перегородками
-#РЕЗУЛЬТАТ: Робот - в исходном положении, и все клетки поля промакированы
-include("library.jl")
-function zakras_with_obstacles(r::Robot)
-    sides = moveAndReturnDirections!(r)
-    marks!(r)
-    moveToStartplace!(r)
-    moveToBeginplace!(r, sides)
-end

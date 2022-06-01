682ec431d53c6b0872da3905f8c01cb28cb05bc7
listova-nastya <90141208+listova-nastya@users.noreply.github.com>
2022-06-01 21:36:18 +0300

    Delete task_4.task_20.jl

--- task_4.task_20.jl
+++
@@ -1,8 +0,0 @@
-#Посчитать число всех горизонтальных прямолинейных перегородок (вертикальных - нет)
-include("library.jl")
-function count_gorizont_obstacles(r)
-    back_path = BackPath(r, (Sud, West))
-    print(counterPartition!(r))
-    BackPath(r, (Sud, West))
-    back!(r, back_path)
-end
